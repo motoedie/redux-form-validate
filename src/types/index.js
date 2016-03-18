15580d78804d67e615b49745faf70b3a21c10fb2
Ladislav Starek <ladislav.starek@avg.com>
2016-03-18 15:13:24 +0100

    Fix missing types

--- src/types/index.js
+++ src/types/index.js
@@ -3,6 +3,8 @@ import createPrimitiveTypeChecker from './createPrimitiveTypeChecker';
 let ValidTypes = {                                // eslint-disable-line prefer-const
   array: createPrimitiveTypeChecker('array'),
   bool: createPrimitiveTypeChecker('boolean'),
+  float: createPrimitiveTypeChecker('float'),
+  integer: createPrimitiveTypeChecker('integer'),
   number: createPrimitiveTypeChecker('number'),
   object: createPrimitiveTypeChecker('object'),
   string: createPrimitiveTypeChecker('string'),
@@ -10,6 +12,8 @@ let ValidTypes = {                                // eslint-disable-line prefer-
 
 ValidTypes.array.isRequired = createPrimitiveTypeChecker('array', true);
 ValidTypes.bool.isRequired = createPrimitiveTypeChecker('boolean', true);
+ValidTypes.float.isRequired = createPrimitiveTypeChecker('float', true);
+ValidTypes.integer.isRequired = createPrimitiveTypeChecker('integer', true);
 ValidTypes.number.isRequired = createPrimitiveTypeChecker('number', true);
 ValidTypes.object.isRequired = createPrimitiveTypeChecker('object', true);
 ValidTypes.string.isRequired = createPrimitiveTypeChecker('string', true);
